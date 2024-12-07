5f42c4eed3bac709658c7fe40f5aede50e1a7128
koko <kawtar.mhf15@gmail.com>
2024-12-07 21:33:29 +0100

    version final test

--- testttt.cpp
+++ testttt.cpp
@@ -6,22 +6,22 @@
 TEST(GrilleTest, ProchaineIteration) {
     Grille grille(3, 3);
 
-    // Initialisation d'un motif simple (barre horizontale)
+    
     grille.getCellule(1, 0).definirEtatSuivant(true);
     grille.getCellule(1, 1).definirEtatSuivant(true);
     grille.getCellule(1, 2).definirEtatSuivant(true);
 
-    // Actualisation des états initiaux
+   
     for (int i = 0; i < grille.getNbLignes(); ++i) {
         for (int j = 0; j < grille.getNbColonnes(); ++j) {
             grille.getCellule(i, j).actualiserEtat();
         }
     }
 
-    // Calculer la prochaine itération
+    
     grille.calculerProchaineIteration();
 
-    // Vérifier les résultats attendus
+  
     EXPECT_TRUE(grille.getCellule(0, 1).estVivante()); // Cellule vivante
     EXPECT_TRUE(grille.getCellule(1, 1).estVivante()); // Cellule vivante
     EXPECT_TRUE(grille.getCellule(2, 1).estVivante()); // Cellule vivante
